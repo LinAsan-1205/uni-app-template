bf5e3b5e152289507cdc8f49846520399a6ad486
林阿三 <>
2023-06-18 22:56:14 +0800

    1

---
+++ pages/components/tag/tag.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<sakura-nav-bar title="Tag"></sakura-nav-bar>
+		<sakura-space :margin="[24,32]">
+			<sakura-tag text="标签一"></sakura-tag>
+			<sakura-tag text="标签二" type="success"></sakura-tag>
+			<sakura-tag text="标签三" type="warning"></sakura-tag>
+			<sakura-tag text="标签四" type="danger"></sakura-tag>
+			<sakura-tag text="标签四" type="purples"></sakura-tag>
+		</sakura-space>
+		<sakura-space :margin="[24,32]">
+			<sakura-tag text="标签一" light></sakura-tag>
+			<sakura-tag text="标签二" type="success" light></sakura-tag>
+			<sakura-tag text="标签三" type="warning" light></sakura-tag>
+			<sakura-tag text="标签四" type="danger" light></sakura-tag>
+			<sakura-tag text="标签四" type="purples" light></sakura-tag>
+		</sakura-space>
+
+		<sakura-space :margin="[24,32]">
+			<sakura-tag text="标签一" plain></sakura-tag>
+			<sakura-tag text="标签二" type="success" plain></sakura-tag>
+			<sakura-tag text="标签三" type="warning" plain></sakura-tag>
+			<sakura-tag text="标签四" type="danger" plain></sakura-tag>
+			<sakura-tag text="标签四" type="purples" plain></sakura-tag>
+		</sakura-space>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ uni_modules/sakura-ui/components/sakura-tag/sakura-tag.vue
@@ -0,0 +1,85 @@
+<template>
+	<view :class="className" :style="stylesName">
+		<slot>
+			{{text}}
+		</slot>
+	</view>
+</template>
+
+<script lang="ts" setup>
+	import { computed, toRefs } from "vue";
+	const props = defineProps({
+		//标签文本	
+		text: {
+			type: String
+		},
+		//标签类型
+		type: {
+			type: String,
+			default: 'primary'
+		},
+		//空心
+		plain: {
+			type: Boolean,
+			default: false
+		},
+		light: {
+			type: Boolean,
+			default: false
+		},
+		//标签文字颜色
+		color: {
+			type: String
+		},
+		//自定义背景颜色
+		background: {
+			type: String
+		},
+		//标签边框颜色	
+		borderColor: {
+			type: String
+		},
+		//标签字体大小，单位rpx
+		size: {
+			type: String,
+			default: '24rpx'
+		},
+		//标签圆角值，单位rpx
+		radius: {
+			type: String,
+			default: '8rpx'
+		},
+		padding: {
+			type: [String, Array],
+			default: []
+		},
+		index: {
+			type: Number,
+			default: 0
+		}
+	})
+
+	const { text, type, plain, color, size, radius, background, borderColor, light, padding } = toRefs(props)
+
+	const { n, classes } = uni.$sakura.utils.createNamespace('tag')
+
+	const className = computed(() => classes(n(), n('--var'), n(`--${type.value}`),
+		[!background.value && plain.value, n(`--${type.value}--plain`)],
+		[!background.value && light.value, n(`--${type.value}--light`)]
+
+	))
+	const getPadding = computed(() => Array.isArray(padding.value) ? padding.value.filter(name => name).map(name => uni.$sakura.utils.getVal(name)).join(' ') : `${padding.value} ${padding.value}`)
+
+	const stylesName = computed(() => ({
+		borderRadius: uni.$sakura.utils.getVal(radius.value),
+		color: color.value,
+		fontSize: uni.$sakura.utils.getVal(size.value),
+		borderColor: borderColor.value,
+		padding: getPadding.value
+	}))
+</script>
+
+<style lang="scss">
+	@import './sakura-tag.scss';
+
+</style>
