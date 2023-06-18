0ed315ff757f16907b2b29c039dbc56e847665d6
林阿三 <>
2023-06-18 21:20:13 +0800

    1

--- pages/components/cell/cell.vue
+++ pages/components/cell/cell.vue
@@ -1,6 +1,25 @@
 <template>
 	<view>
 		<sakura-nav-bar title="Cell"></sakura-nav-bar>
+		<sakura-space :margin="[24,32]">
+			<sakura-cell title="这是单元格" value="内容" leftIcon="github"></sakura-cell>
+			<sakura-cell title="这是单元格" value="内容" leftIcon="github"></sakura-cell>
+		</sakura-space>
+
+		<sakura-space :margin="[24,32]">
+			<sakura-cell title="这是边框单元格" value="内容" leftIcon="github" border></sakura-cell>
+			<sakura-cell title="这是边框单元格" value="内容" leftIcon="github" border></sakura-cell>
+		</sakura-space>
+
+		<sakura-space :margin="[24,32]">
+			<sakura-cell title="这是箭头单元格" arrow border></sakura-cell>
+			<sakura-cell title="这是箭头跳转单元格" arrow isLink url="/pages/components/icon/icon" border></sakura-cell>
+		</sakura-space>
+
+		<sakura-space :margin="[24,32]">
+			<sakura-cell title="这是单元格" value="内容" :center="false" leftIcon="github" description="这是一个描述"></sakura-cell>
+			<sakura-cell title="这是居中单元格" value="内容" center leftIcon="github" description="这是一个描述"></sakura-cell>
+		</sakura-space>
 	</view>
 </template>
 
--- uni_modules/sakura-ui/components/sakura-cell/sakura-cell.vue
+++ uni_modules/sakura-ui/components/sakura-cell/sakura-cell.vue
@@ -1,11 +1,122 @@
 <template>
-	<view :class="className"></view>
+	<view :class="className" @click="onCell">
+		<slot name="icon">
+			<view :class="classes(n('--icon'))" v-if="leftIcon">
+				<sakura-icon :name="leftIcon"></sakura-icon>
+			</view>
+		</slot>
+		<view :class="classes(n('--content'))">
+			<slot>
+				<view :class="classes(n('--title'))" v-if="title">
+					{{title}}
+				</view>
+			</slot>
+			<slot name="description">
+				<view :class="classes(n('--description'))" v-if="description">
+					{{description}}
+				</view>
+			</slot>
+		</view>
+		<view :class="classes(n('--right'))">
+			<slot name="right">
+				<slot name="value">
+					<text :class="classes(n('--value'))">{{value}}</text>
+				</slot>
+				<sakura-icon :name="rightIcon" v-if="props.rightIcon||arrow" @click.stop="onRightIcon"></sakura-icon>
+			</slot>
+		</view>
+	</view>
 </template>
 
 <script lang="ts" setup>
-	import { computed } from "vue";
+	import { computed, toRefs, PropType } from "vue";
+
+	const emit = defineEmits(['click', 'rightIcon'])
+
+	const props = defineProps({
+
+		//单元格标题	
+		title: {
+			type: String as PropType<string>,
+		},
+		//描述信息	
+		description: {
+			type: String as PropType<string>,
+		},
+
+		//右侧的内容
+		value: {
+			type: String as PropType<string>,
+		},
+
+		//是否显示下边框	
+		border: {
+			type: Boolean as PropType<boolean>,
+			default: false
+		},
+		//左侧icon
+		leftIcon: {
+			type: String as PropType<string>,
+		},
+		//整体垂直居中
+		center: {
+			type: Boolean as PropType<boolean>,
+			default: true
+		},
+		//是否展示右侧箭头
+		arrow: {
+			type: Boolean as PropType<boolean>,
+			default: false
+		},
+		//右侧icon
+		rightIcon: {
+			type: String as PropType<string>,
+		},
+		//右侧icon 点击事件
+		rightIconClick: {
+			type: Boolean as PropType<boolean>,
+			default: false
+		},
+		isLink: {
+			type: Boolean as PropType<boolean>,
+			default: false
+		},
+		//
+		url: {
+			type: String as PropType<string | any>,
+		}
+	})
+
+	const { title, description, value, border, leftIcon, center, arrow, rightIconClick, isLink, url } = toRefs(props)
+
 	const { n, classes } = uni.$sakura.utils.createNamespace('cell')
-	const className = computed(() => classes(n()))
+
+	const className = computed(() => classes(n(), n('--var'), [border.value, n('--border')], [center.value, n('--center')]))
+
+	const rightIcon = computed(() => {
+		if (arrow.value) {
+			return 'arrow-right';
+		}
+		return props.rightIcon
+	})
+
+	const onCell = () => {
+		if (isLink.value && url.value) {
+			uni.navigateTo({
+				url: url.value
+			})
+			return
+		}
+		emit('click')
+	}
+	const onRightIcon = () => {
+
+		if (rightIconClick.value) {
+			emit('rightIcon')
+			return
+		}
+		onCell()
+	}
 </script>
 
 <style lang="scss">
