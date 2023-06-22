d0e2cdb45b23753bd4125ca13d7e9fd9bbb2332c
林阿三 <>
2023-06-22 12:34:58 +0800

    1

--- uni_modules/sakura-ui/components/sakura-message/sakura-message.vue
+++ uni_modules/sakura-ui/components/sakura-message/sakura-message.vue
@@ -1,5 +1,5 @@
 <template>
-	<view :class="className">
+	<view :class="className" :style="customNavTop">
 		<sakura-transition :zIndex="1000" :duration="300" mode-class="slide-top" :show="showMessage">
 			<view :class="classes(n('--wrapper'))">
 				<view :style="contentStyle" :class="classes(n('--content'),[classType,n(`--${classType}`)])">
@@ -12,8 +12,23 @@
 	</view>
 </template>
 
-<script setup>
-	import { computed, ref, watch } from 'vue'
+<script setup lang="ts">
+	import { computed, ref, watch, PropType } from 'vue'
+
+	const props = defineProps({
+		//适配自定义导航
+		customNavBar: {
+			type: Boolean as PropType<boolean>,
+			default: true
+		},
+		//自定义导航高度
+		customNavBarHeight: {
+			type: Number as PropType<number>,
+			default: () => {
+				return uni.$sakura.config.navbar.height
+			}
+		}
+	})
 
 	const { n, classes } = uni.$sakura.utils.createNamespace('message')
 
@@ -31,9 +46,17 @@
 
 	const className = computed(() => classes(n(), n('--var')))
 
+
 	const contentStyle = ref({})
 
-	const open = (text, val) => {
+	const customNavTop = computed(() => {
+		if (!props.customNavBar) return {}
+		return {
+			top: uni.$sakura.utils.getCustomNavTop(props.customNavBarHeight)
+		}
+	})
+
+	const open = (text : string, val : string) => {
 		if (!val || !type.includes(val)) {
 			console.error('缺少类型：', val)
 			return
@@ -50,12 +73,12 @@
 		timerOut()
 	}
 
-	const show = (config = {}) => {
+	const show = (config : Record<string, any>) => {
 		const {
 			type = "primary", color = "#fff", bgColor = null, message: text, time = 3000, fontSize =
-				'32rpx'
+			'32rpx'
 		} =
-		config
+			config
 
 		message.value = text
 		duration.value = time
@@ -68,7 +91,7 @@
 			return
 		}
 		if (type) {
-			classType = type
+			classType.value = type
 		}
 		showMessage.value = true
 		timerOut()
@@ -93,7 +116,7 @@
 	})
 
 	const typeMethods = type.reduce((a, c) => {
-		a[c] = (text) => open(text, c)
+		a[c] = (text : string) => open(text, c)
 		return a
 	}, {})
 
@@ -102,7 +125,6 @@
 		...typeMethods,
 		close
 	})
-
 </script>
 
 <style lang="scss">
