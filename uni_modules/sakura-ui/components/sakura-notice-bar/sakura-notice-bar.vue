aea921e17138ea5a216af5968bdc0a5b1eec34e6
林阿三 <>
2023-06-24 01:11:23 +0800

    1

---
+++ uni_modules/sakura-ui/components/sakura-notice-bar/sakura-notice-bar.vue
@@ -0,0 +1,24 @@
+<template>
+	<view :class="className"></view>
+</template>
+
+
+<!-- #ifdef MP-WEIXIN -->
+<script lang="ts">
+	export default {
+		options: { virtualHost: true }
+	}
+</script>
+<!-- #endif -->
+<script setup lang="ts">
+	import { computed } from "vue";
+
+	const { n, classes } = uni.$sakura.utils.createNamespace('notice')
+
+	const className = computed(() => classes(n(), n('--var')))
+</script>
+
+<style lang="scss">
+	@import './sakura-notice-bar.scss';
+
+</style>
