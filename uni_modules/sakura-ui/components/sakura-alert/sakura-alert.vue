0673d2c05efe3aa7aa7b686efc964c0bd051c828
林阿三 <>
2023-06-24 16:18:03 +0800

    1

---
+++ uni_modules/sakura-ui/components/sakura-alert/sakura-alert.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+
+<!-- #ifdef MP-WEIXIN -->
+<script lang="ts">
+	export default {
+		options: { virtualHost: true }
+	}
+</script>
+<!-- #endif -->
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss">
+	@import './sakura-alert.scss';
+
+</style>
