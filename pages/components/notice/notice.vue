34c547da4e8c4ea3edab33894a28a5c80761cf63
林阿三 <>
2023-06-24 18:00:48 +0800

    1

--- pages/components/notice/notice.vue
+++ pages/components/notice/notice.vue
@@ -2,9 +2,10 @@
 	<view>
 		<sakura-nav-bar title="NoticeBar"></sakura-nav-bar>
 		<sakura-space vertical>
-			<sakura-notice-bar text="注意，这是一个提醒消息" show-icon></sakura-notice-bar>
+			<sakura-notice-bar
+				text="uni-app 版正式发布，开发一次，同时发布iOS、Android、H5、微信小程序、支付宝小程序、百度小程序、头条小程序等7大平台。"></sakura-notice-bar>
 
-			<sakura-notice-bar text="注意，这是一个提醒消息" showClose></sakura-notice-bar>
+			<sakura-notice-bar text="注意，这是一个提醒消息" scrollable></sakura-notice-bar>
 		</sakura-space>
 	</view>
 </template>
--- uni_modules/sakura-ui/components/sakura-notice-bar/sakura-notice-bar.vue
+++ uni_modules/sakura-ui/components/sakura-notice-bar/sakura-notice-bar.vue
@@ -6,10 +6,26 @@
 					size="38rpx"></sakura-icon>
 			</slot>
 		</view>
-		<view :class="classes(n('--content'))">{{text}}</view>
-		<view :class="classes(n('--right'))">
+		<view :class="classes(n('--content'))" v-if="direction==='row'">
+			<view ref="animationEl" :class="classes(n('--text'),[
+				scrollable,
+				n('--scrollable')
+			],
+			 [!scrollable,n('--single')]
+			)" :style="{
+						'animationDuration': animation.duration,
+						'-webkit-animationDuration': animation.duration,
+						animationPlayState: webviewHide ? 'paused' : animation.playState,
+						'-webkit-animationPlayState': webviewHide ? 'paused' : animation.playState,
+						animationDelay: animation.delay,
+						'-webkit-animationDelay': animation.delay
+					}">
+				{{text}}
+			</view>
+		</view>
+		<view :class="classes(n('--right'))" v-if="showClose">
 			<slot name="right">
-				<sakura-icon v-if="rightIcon&&showClose" :name="rightIcon" :color="rightIconColor||getVar('-bar-color')"
+				<sakura-icon v-if="rightIcon" :name="rightIcon" :color="rightIconColor||getVar('-bar-color')"
 					size="38rpx"></sakura-icon>
 			</slot>
 		</view>
@@ -25,7 +41,14 @@
 </script>
 <!-- #endif -->
 <script setup lang="ts">
-	import { computed, toRefs } from "vue";
+	import { computed, nextTick, onBeforeUnmount, onMounted, reactive, ref, toRefs, getCurrentInstance } from "vue";
+
+	// #ifdef APP-NVUE
+	const dom = weex.requireModule('dom');
+	const ani = weex.requireModule('animation');
+	// #endif
+
+	const instance = getCurrentInstance()
 
 	const props = defineProps({
 		//内容
@@ -35,7 +58,7 @@
 		//显示左侧图标
 		showIcon: {
 			type: Boolean,
-			default: false
+			default: true
 		},
 		//左侧图标
 		leftIcon: {
@@ -59,14 +82,103 @@
 		//右侧图标颜色
 		rightIconColor: {
 			type: String
+		},
+		//滚动模式 通告滚动模式，row-横向滚动，column-竖向滚动	
+		direction: {
+			type: String,
+			default: 'row'
+		},
+		//动画时间
+		startTime: {
+			type: Number,
+			default: 1000
+		},
+		speed: {
+			type: Number,
+			default: 100
+		},
+		//是否滚动
+		scrollable: {
+			type: Boolean,
+			default: false
 		}
 	})
 
-	const { text, showIcon, leftIcon, leftIconColor, rightIcon, rightIconColor, showClose } = toRefs(props)
+	const { text, showIcon, leftIcon, leftIconColor, rightIcon, rightIconColor, showClose, direction, startTime, speed, scrollable } = toRefs(props)
 
 	const { n, classes, getVar } = uni.$sakura.utils.createNamespace('notice')
 
 	const className = computed(() => classes(n(), n('--var')))
+
+	const webviewHide = ref(false)
+
+	const animation = reactive({
+		duration: '0',
+		playState: 'paused',
+		delay: '0s'
+	})
+
+	const animationEl = ref()
+
+
+	// #ifdef APP-NVUE
+	const stopAnimation = ref(false)
+	// #endif
+
+	const getWidth = (val : string) => {
+		return new Promise((resolve) => {
+			uni.createSelectorQuery()
+				// #ifndef MP-ALIPAY
+				.in(instance)
+				// #endif
+				.select(`.${val}`)
+				.boundingClientRect()
+				.exec(ret => {
+					resolve(ret[0].width)
+				})
+		})
+	}
+
+	const initSize = () => {
+		if (scrollable.value) {
+			// #ifndef APP-NVUE
+
+			Promise.all([getWidth(n('--content')), getWidth(n('--text'))]).then((res : any) => {
+				console.log(res[1], '1')
+				animation.duration = `${res[1] / speed.value}s`
+				animation.delay = `-${res[0] / speed.value}s`
+
+				setTimeout(() => {
+					animation.playState = 'running'
+				}, startTime.value)
+			})
+			// #endif
+
+
+		}
+	}
+
+	onMounted(() => {
+		// #ifdef APP-PLUS
+		var pages = getCurrentPages();
+		var page = pages[pages.length - 1];
+		var currentWebview = page.$getAppWebview();
+		currentWebview.addEventListener('hide', () => {
+			webviewHide.value = true
+		})
+		currentWebview.addEventListener('show', () => {
+			webviewHide.value = false
+		})
+		// #endif
+		nextTick(() => {
+			initSize()
+		})
+	})
+	// #ifdef APP-NVUE
+	onBeforeUnmount(() => {
+		stopAnimation.value = true
+	})
+	// #endif
 </script>
 
 <style lang="scss">
