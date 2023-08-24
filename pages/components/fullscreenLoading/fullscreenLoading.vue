73a55b937a4790e6d424828d3f0c0450d97234e7
linasan <1603889221@qq.com>
2023-08-24 22:30:44 +0800

    1

--- pages/components/fullscreenLoading/fullscreenLoading.vue
+++ pages/components/fullscreenLoading/fullscreenLoading.vue
@@ -1,16 +1,20 @@
 <template>
 	<sakura-nav-bar title="全屏加载"></sakura-nav-bar>
+	<sakura-button type="primary" block @click="showLoading">显示</sakura-button>
+	<sakura-fullscreen-loading :loading="loading"></sakura-fullscreen-loading>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
+<script setup lang="ts">
+	import { ref } from 'vue'
+	const loading = ref(false)
 
-			};
-		}
+	const showLoading = () => {
+		loading.value = true
+
+		setTimeout(() => {
+			loading.value = false
+		}, 3000)
 	}
-
 </script>
 
 <style lang="scss">
--- uni_modules/sakura-ui/components/sakura-fullscreen-loading/sakura-fullscreen-loading.vue
+++ uni_modules/sakura-ui/components/sakura-fullscreen-loading/sakura-fullscreen-loading.vue
@@ -1,7 +1,41 @@
 <template>
+	<view :class="className" :style="styles" v-if="loading">
+		<sakura-loading :text="text"></sakura-loading>
+	</view>
 </template>
 
 <script lang="ts" setup>
+	import { computed, toRefs } from 'vue'
+
+	const props = defineProps({
+		loading: {
+			type: Boolean,
+			default: false
+		},
+		text: {
+			type: String,
+			default: '加载中...'
+		},
+		zIndex: {
+			type: [Number, String],
+			default: 9999
+		},
+		background: {
+			type: String,
+			default: 'rgba(255,255,255,.6)'
+		}
+	})
+
+	const { loading, text, zIndex, background } = toRefs(props)
+
+	const { n, classes } = uni.$sakura.utils.createNamespace('fullscreen-loading')
+
+	const className = computed(() => classes(n(), n('--var')))
+
+	const styles = computed(() => ({
+		zIndex: zIndex.value,
+		background: background.value
+	}))
 </script>
 
 <style lang="scss">
