f484ffcdaad07dc21837815ab4b5ef8a80e4caad
linasan <1603889221@qq.com>
2023-09-13 18:06:23 +0800

    1

---
+++ uni_modules/sakura-ui/components/sakura-stepper/sakura-stepper.vue
@@ -0,0 +1,19 @@
+<template>
+	<view :class="className">
+		<slot name="min"></slot>
+
+		<slot name="max"></slot>
+	</view>
+</template>
+
+<script lang="ts" setup>
+	import { computed } from "vue";
+	const { n, classes } = uni.$sakura.utils.createNamespace('stepper')
+
+	const className = computed(() => classes(n(), n('--var')))
+</script>
+
+<style lang="scss">
+	@import './sakura-stepper.scss';
+
+</style>
